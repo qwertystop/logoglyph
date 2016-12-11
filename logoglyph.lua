8d97f1a0df2836f6c25fbf496beec14e78bd9218
David Heyman <d.heyman256@gmail.com>
2016-12-10 22:55:53 -0500

    Added lines, regular polygons, plan for multilines

--- logoglyph.lua
+++ logoglyph.lua
@@ -32,30 +32,73 @@ SkewY = simpleclass{ang = 0}
 -- All instances of a shape have the same base parameters,
 -- and are modified from there by transforms only.
 -- Anchors are used in shifting shapes to align
+-- All shapes have a getanchortransform function, returning a random point on the edge
+-- or in the center of the shape, in coordinates local to that shape's primitive.
+-- If a shape is instantiated with params other than the default, getanchortransform
+-- may not align to the shape.
 ---------------
 -------
 -- Circles
--- Center and radius. Anchor is center or edge.
+-- Primitive: Center at origin, radius 1.
+-- No ellipse primitive - emergent, just unevenly scale a circle.
 -------
-Circle = simpleclass{params = {center = {0, 0}, radius = 1},
-                     transforms = {}}
-Circle.__index = Circle
-function Circle:new(transforms)
-	return setmetatable({transforms = transforms}, self)
-end
+Circle = simpleclass{params = {cx = 0, cy = 0, r = 1},
+			transforms = {}}
 
--- 1/centerodds chance of selecting center
--- returns a translation putting {0, 0} on that point
-function Circle:getanchortransform(centerodds)
+
+-- Selects a point from center or any point on edge
+-- 1/centerodds chance of selecting center (thus, configurable)
+-- Equal chance of any point on edge to any other point, if not center
+-- Returns a translation putting (0, 0) on selected point
+function Circle.getanchortransform(centerodds)
 	local rand = math.random(centerodds)
 	if rand == 1 then
 		return Translate:new(0, 0)
 	else
-		local angle = math.random() * math.pi / 2
+		local angle = math.random() * math.pi * 2
 		return Translate:new{dx = math.cos(angle), dy = math.sin(angle)}
 	end
 end
 
--- TODO Repeat for other primitives.
+-------
+-- Line
+-- Primitive: (0, 0) to (1, 0)
+-------
+Line = simpleclass{params = {x1 = 0, y1 = 0; x2 = 1, y2 = 0},
+		transforms = {}}
 
+-- Selects a random point on the line
+-- Since everything is a transform from (1, 0), this is simple.
+function Line.getanchortransform()
+	return Translate:new{dx = math.random(), dy = 0}
+end
 
+-------
+-- RegPolygon
+-- Points on a radius-1 circle
+-- Anchors: Corners or center
+-- TODO extend to all-points-on-shape
+-- Actually a class-factory - input side number, get class for that many sides
+-------
+RegPolygon = simpleclass{params = {sides = sides, x = 0, y = 0},
+		transforms = {}}
+
+-- Selects a random corner of the shape, or the center
+-- Equal chance of all corners, 1/centerodds chance of center
+function RegPolygon:getanchortransform(centerodds)
+	local rand = math.random(centerodds)
+	if rand == 1 then
+		return Translate:new{0, 0}
+	else
+		local angle = ((math.pi * 2) 
+			/ (math.random(self.params.sides) / self.params.sides))
+		return Translate:new{dx = math.cos(angle), dy = math.cos(angle)}
+	end
+end
+-------
+-- Polyline
+-- TODO get the main functionality working first
+-- Lines specifically connected at ends
+-- Anchor at corner or end
+-- Random angle, random length within bounds
+-------
