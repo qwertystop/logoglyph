5dff23f4d7ae25967fffae8fd2b6119fd05c921d
David Heyman <d.heyman256@gmail.com>
2016-12-10 21:59:54 -0500

    Lifted notes to sep. file

--- logoglyph.lua
+++ logoglyph.lua
@@ -1,13 +1,5 @@
---[[
-libraries for producing SVG-compliant XML:
-genx.luaforge.net
-effectively streams it - start element, add attributes and content, end element.
 
-Alternately, etree.luaforge.net
-converts a table in one go rather than streaming
 
-still unsure which - see which way the code tends towards.
-]]--
 
 -- Classes for primitive shapes
 Circle = {params = {origin = 0, radius = 1},
