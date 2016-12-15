2e6fd005f04118ae99e7b704d04526fc7451c9d8
David Heyman <d.heyman256@gmail.com>
2016-12-14 21:57:59 -0500

    Convert to use penlight

--- logoglyph.lua
+++ logoglyph.lua
@@ -7,133 +7,128 @@
 ---------------
 -- Requirements
 ---------------
-local lfs = require "lfs" -- luafilesystem
-local argparse = require "argparse"
+local pl = require "pl.import_into"() -- penlight
 
 ---------------
+-- Help
+---------------
+local function help()
+	local info = {
+		"Logoglyph: A script producing random SVGs from \n"
+		.. "primitive shapes with chained transformations.",
+		"\n",
+		"Arguments (all 'chances' are 0 < arg < 1):",
+		"--quantity:\tHow many images to write?",
+		"--morehapes:\tChance of adding another shape at each step.",
+		"--centerodds:\tChance of centering a new shape in a previous shape,\n"
+		.. "\t\tinstead of putting it on the edge.",
+		"--moretransforms:\tChance of chaining another transform after adding one.",
+		"--translate:\tPortion of transformations to be translations\n"
+		.."\t\t(relative to other four)",
+		"--rotate:\tPortion of transformations to be rotations\n"
+		.."(\t\trelative to other four)",
+		"--scale:\tPortion of transformations to be scales\n"
+		.."\t\t(relative to other four)",
+		"--skewx:\tPortion of transformations to be skews on the X direction\n"
+		.."\t\t(relative to other four)",
+		"--skewy:\tPortion of transformations to be skews on the Y direction\n"
+		.."\t\t(relative to other four)",
+		"--output:\tDirectory to write files into"
+	}
+	print(table.concat(info, '\n'))
+end
+
+--------------
 -- Utilities
----------------
--- Simple classmaking function, set up index and meta
--- Does not provide for inheritance
--- Does provide for (extremely simplified) type-checking
-local function simpleclass(class, typename)
-	class.__index = class
-	class.name = typename
-	class.is_a = function(self, askname) return self.name == askname end
-	class.new = function(self, obj)
-		local obj = obj or {}
-		return setmetatable(obj, class)
-	end
-	return class
-end
-
--- Lazy initializer for empty-table fields
-local function lazyInit(paramname)
-	return function(self)
-		local _t = {} -- auto-initialize to new empty table
-		self[paramname] = function(self) return _t end
-		return _t
-	end
-end
-
+--------------
 local function sum(tab)
-	local _sum = 0
-	for key, val in pairs(tab) do
-		_sum = _sum + val
+	local sum = 0
+	for _, v in pairs(tab) do
+		sum = sum + v
 	end
-	return _sum
+	return sum
 end
-
-local function partial(f, arg)
-	return function(...)
-		return f(arg, ...)
-	end
-end
-
----------------
--- Arguments
----------------
-local parser = argparse()
-	:name "Logoglyph v0.1"
-	:description "Constructs small patterns randomly, as SVG, by repeated transformation of primitives"
-parser:option "-q --quantity"
-	:convert(tonumber)
-	:description "How many images to draw?"
-
-parser:option "-s --moreshapes"
-	:convert(tonumber)
-	:description "Odds of adding another component shape after each addition (vs. stopping)  (0 <= m < 1)"
-
-parser:option "-c --centerodds"
-	:convert(tonumber)
-	:description "Odds of anchoring a shape in the center of another (vs. an edge) (0 <= c <= 1)"
-
-parser:option "-C --continue"
-	:convert(tonumber)
-	:description "Odds of adding another chained transformation after each addition (vs. stopping)  (0 <= m < 1)"
-
-
-for key, val in pairs{"translate", "scale", "rotate", "skewx", "skewy"} do
-	parser:option("--" .. val)
-		:convert(tonumber)
-		:description("Likelihood of choosing this transformation "
-				.. "when applying a transformation, "
-				.. "relative to the other four.")
-end
-
-parser:option "-t --translatemax"
-	:convert(tonumber)
-	:description "Maximum distance for one translation"
-
-parser:option "-r --scalemax"
-	:convert(tonumber)
-	:description "Maximum magnitude of scaling"
-
-parser:option "-o --output"
-	:description "Directory to write files to (default writes everything to stdout)"
 
 ---------------
 -- Classes for different transforms
 ---------------
 -- Random initializers are common to multiple transforms
-local function randAngleInit(self)
-	return self:new{ang = math.random() * 2 * math.pi}
-end
+local TwoArgTransform = pl.class {
+	_name = "twoArgTransform";
 
-local function twoRandInit(self, magnitude)
-	return self:new{x = math.random() * 2 * magnitude - magnitude,
-			y = math.random() * 2 * magnitude - magnitude}
-end
+	_init = function(self, x, y)
+		self.x = x
+		self.y = y
+	end;
 
-local Translate = simpleclass({x = 0, y = 0}, "Translate")
-Translate.newRand = twoRandInit
-function Translate:asSVGAttribute()
-	return ('transform="translate(' .. self.x .. ',' .. self.y .. ')"')
-end
+	newRand = function (self)
+			return self:init{
+				x = math.random() * 2 * 10 - 10,
+				y = math.random() * 2 * 10 - 10}
+	end;
+}
 
-local Scale = simpleclass({x = 0, y = 0}, "Scale")
-Scale.newRand = twoRandInit
-function Scale:asSVGAttribute()
-	return ('transform="scale(' .. self.x .. ',' .. self.y .. ')"')
-end
+local Translate = pl.class {
+	_name = "Translate";
 
-local Rotate = simpleclass({ang = 0}, "Rotate") -- remember SVG uses degrees but Lua uses radians
-Rotate.newRand = randAngleInit
-function Rotate:asSVGAttribute()
-	return ('transform="rotate(' .. self.ang .. ')"')
-end
+	_base = TwoArgTransform;
 
-local SkewX = simpleclass({ang = 0}, "SkewX")
-SkewX.newRand = randAngleInit
-function SkewX:asSVGAttribute()
-	return ('transform="skewX(' .. self.ang .. ')"')
-end
+	asSvgAttribute = function (self)
+		return ('transform="translate(' .. self.x .. ',' .. self.y .. ')"')
+	end
+}
 
-local SkewY = simpleclass({ang = 0}, "SkewY")
-SkewY.newRand = randAngleInit
-function SkewY:asSVGAttribute()
-	return ('transform="skewY(' .. self.ang .. ')"')
-end
+local Scale = pl.class {
+	_name = "Scale";
+
+	_base = TwoArgTransform;
+
+	asSvgAttribute = function (self)
+		return ('transform="scale(' .. self.x .. ',' .. self.y .. ')"')
+	end
+}
+
+local oneArgTransform = pl.class {
+	_name = "oneArgTransform";
+
+	_init = function (self, ang)
+		self.ang = ang
+	end;
+
+	newRand = function (self)
+		return self:init{ang = math.random() * 2 * math.pi}
+	end
+}
+
+local Rotate = pl.class {
+	_name = "Rotate";
+
+	_base = oneArgTransform;
+
+	asSvgAttribute = function (self)
+		return ('transform="rotate(' .. self.ang .. ')"')
+	end
+}
+
+local SkewX = pl.class {
+	_name = "SkewX";
+
+	_base = oneArgTransform;
+
+	asSvgAttribute = function (self)
+		return ('transform="skewX(' .. self.ang .. ')"')
+	end
+}
+
+local SkewY = pl.class {
+	_name = "SkewY";
+
+	_base = oneArgTransform;
+
+	asSvgAttribute = function (self)
+		return ('transform="skewY(' .. self.ang .. ')"')
+	end
+}
 
 ---------------
 -- Classes for primitive shapes
@@ -145,88 +140,124 @@ end
 -- If a shape is instantiated with params other than the default, getAnchorTransform
 -- may not align to the shape.
 ---------------
+
+local Shape = pl.class{
+	_init = function (self)
+		self.transforms = pl.List.new()
+		self.children = pl.List.new()
+	end
+}
+
 -------
 -- Circles
 -- Primitive: Center at origin, radius 100.
 -- No ellipse primitive - emergent, just unevenly scale a circle.
 -------
-local Circle = simpleclass({params = {cx = 0, cy = 0, r = 100},
-		getTransforms = lazyInit "getTransforms",
-		getChildren = lazyInit "getChildren"},
-		"Circle")
+local Circle = pl.class{
+	_name = "Circle";
+	
+	_base = Shape;
+	
+	_init = function (self)
+		self:super()
+		self.cx = 0
+		self.cy = 0
+		self.r = 10
+	end;
+	
+	-- centerodds/1 chance of selecting center (thus, configurable)
+	-- Equal chance of any point on edge to any other point, if not center
+	-- Returns a translation putting (0, 0) on selected point
+	getAnchorTransform = function (self, centerodds)
+		if math.random() < centerodds then
+			return Translate(0, 0)
+		else
+			local angle = math.random() * math.pi * 2
+			return Translate(math.cos(angle) * 100,
+					math.sin(angle) * 100)
+		end
+	end;
 
+	asSVGElement = function (self)
+		return '<use xlink:href"#BaseCircle"/>'
+	end;
+}
 
--- Selects a point from center or any point on edge
--- centerodds/1 chance of selecting center (thus, configurable)
--- Equal chance of any point on edge to any other point, if not center
--- Returns a translation putting (0, 0) on selected point
-function Circle:getAnchorTransform(centerodds)
-	if math.random() < centerodds then
-		return Translate:new{x = 0, y = 0}
-	else
-		local angle = math.random() * math.pi * 2
-		return Translate:new{x = math.cos(angle) * 100, y = math.sin(angle) * 100}
-	end
-end
-
-function Circle:asSVGElement()
-	return '<use xlink:href"#BaseCircle"/>'
-end
 
 -------
 -- Line
 -- Primitive: (0, 0) to (100, 0)
 -------
-local Line = simpleclass({params = {x1 = 0, y1 = 0; x2 = 100, y2 = 0},
-		getTransforms = lazyInit "getTransforms",
-		getChildren = lazyInit "getChildren"},
-		"Line")
+local Line = pl.class{
+	_name = "Line";
 
--- Selects a random point on the line
--- Since everything is a transform from (100, 0), this is simple.
--- Consumes one argument to keep a consistent interface, ignores it.
-function Line:getAnchorTransform(_)
-	return Translate:new{x = math.random() * 100, y = 0}
-end
+	_base = Shape;
 
-function Line:asSVGElement()
-	return '<use xlink:href"#BaseCircle"/>'
-end
+	_init = function (self)	
+		self:super()
+		self.x1 = 0
+		self.y1 = 0
+		self.x2 = 100
+		self.y2 = 0
+	end;
+
+	-- Selects a random point on the line
+	-- Since everything is a transform from (100, 0), this is simple.
+	-- Consumes one argument to keep a consistent interface, ignores it.
+	getAnchorTransform = function (self, _)
+		return Translate(math.random() * 100, 0)
+	end;
+
+	asSVGElement = function (self)
+		return '<use xlink:href"#BaseCircle"/>'
+	end;
+}
 
 -------
 -- RegPolygon
 -- Points on a radius-100 circle
 -- Anchors: Corners or center
--- TODO extend to all-points-on-shape? Maybe, maybe not.
+-- TODO anchor possibilities extend to all-points-on-shape? Maybe, maybe not.
 -------
-local RegPolygon = simpleclass({params = {sides = sides, x = 0, y = 0},
-		getTransforms = lazyInit "getTransforms",
-		children = lazyInit "getChildren"},
-		"RegPolygon")
+local RegPolygon = pl.class{
+	_name = "Line";
 
--- Selects a random corner of the shape, or the center
--- Equal chance of all corners, centerodds/1 chance of center
-function RegPolygon:getAnchorTransform(centerodds)
-	if math.random() < centerodds then
-		return Translate:new{0, 0}
-	else
-		local angle = ((math.pi * 2) 
-			/ (math.random(self.params.sides) / self.params.sides))
-		return Translate:new{x = math.cos(angle) * 100, y = math.sin(angle) * 100}
+	_base = Shape;
+	
+	_init = function (self)
+		self:super()
+		self.cx = 0
+		self.cy = 0
+		self.sides = math.random(10)
+	end;
+
+	-- Selects a random corner of the shape, or the center
+	-- Equal chance of all corners, centerodds/1 chance of center
+	getAnchorTransform = function (centerodds)
+		if math.random() < centerodds then
+			return Translate(0, 0)
+		else
+			local angle = ((math.pi * 2) 
+				/ (math.random(self.sides) / self.sides))
+			return Translate(math.cos(angle) * 100,
+					math.sin(angle) * 100)
+		end
+	end;
+
+	 asSVGElement = function (self)
+		local pointset = {}
+		for i = 0, (self.params.sides - 1) do
+			angle = (math.pi * 2) / (i / self.params.sides)
+			table.insert(pointset, tostring(math.cos(angle) * 100))
+			table.insert(pointset, tostring(math.sin(angle) * 100))
+		end
+		return table.concat({
+			'<polygon fill="none" stroke="black" stroke-width="10" points="', 
+			table.concat(pointset, ' '),
+			'"/>'
+		}, '')
 	end
-end
-
-function RegPolygon:asSVGElement()
-	local pointset = {}
-	for i = 0, (self.params.sides - 1) do
-		angle = (math.pi * 2) / (i / self.params.sides)
-		table.insert(pointset, tostring(math.cos(angle) * 100))
-		table.insert(pointset, tostring(math.sin(angle) * 100))
-	end
-	return table.concat({'<polygon fill="none" stroke="black" stroke-width="10" points="', 
-		table.concat(pointset, ' '), '"/>'}, '')
-end
-
+}
 -------
 -- Polyline
 -- TODO get the main functionality working first
@@ -242,35 +273,30 @@ end
 -- Create a random chain of transforms
 -- Weights provided by argument
 -- odds of any given transform (n / sum, must be ints),
--- continue: likelihood of continuing (n / 1)
+-- moretransforms: likelihood of continuing (n / 1)
 -- weights = {translate = n1, scale = n2, rotate = n3, skewx = n4, skewy = n5}
-local function randTransform(weights, continue, translatemax, scalemax)
-	local funList = {}
-	for i = 1, weights.translate do
-		table.insert(funList, partial(
-				partial(Translate.newRand, Translate),
-				translatemax))
-	end
-	for i = 1, weights.scale do
-		table.insert(funList, partial(
-				partial(Scale.newRand, Scale),
-				scalemax))
-	end
-	for key, val in pairs{rotate = Rotate, skewx = SkewX, skewy = SkewY} do
+local function randTransform(weights, moretransforms)
+	local funList = pl.List.new()
+	for key, val in pairs {
+			translate = Translate,
+			scale = Scale,
+			rotate = Rotate,
+			skewx = SkewX,
+			skewy = SkewY} do
 		for i = 1, weights[key] do
-			table.insert(funList, partial(val.newRand, val))
+			funList:append(pl.func.bind1(val.newRand, val))
 		end
 	end
 
 	local weightsum = sum(weights)
 	assert(#funList == weightsum, "list " .. #funList .. " sum " .. weightsum)
 
-	local transformList = {}
-	while math.random() < continue do
+	local transformList = pl.List.new()
+	while math.random() < moretransforms do
 		local selection = math.random(weightsum)
-		local item = nil
-		table.insert(transformList, funList[selection]())
+		transformList:append(funList[selection]())
 	end
+
 	return transformList
 end
 
@@ -282,36 +308,33 @@ end
 -- centerodds required, certain shapes use it
 local function randShape(centerodds, othershape)
 	local shapes = {Circle, Line, RegPolygon}
-	local base = shapes[math.random(3)]:new({})
-	print(base.name)
-	table.insert(base:getTransforms(),
-			base:getAnchorTransform(centerodds))
+	local base = shapes[math.random(3)]()
+	print(base._name)
+	base.transforms:append(base:getAnchorTransform(centerodds))
 	if othershape then
-		table.insert(base:getTransforms(),
-			othershape:getAnchorTransform(centerodds))
-		table.insert(othershape:getChildren(), base)
+		base.transforms:append(othershape:getAnchorTransform(centerodds))
+		othershape.children:append(base)
 	end
 	return base
 end
 
 -- Create a full scenegraph (returns root).
 -- Pass a table containing configuration:
-
+-- 'moretransforms' = odds of adding another transform, (n / 1)
 -- 'moreshapes' = odds of adding another shape, (n / 1)
 -- 'centerodds' (see randShape)
--- and all parameters to randTransform (weights, continue, translatemax, scalemax)
+-- and weights for randTransform
 local function makeScenegraph(config)
 	-- Minimum two shapes
-	local shapes = {}
+	local shapes = pl.List.new()
 	table.insert(shapes, randShape(config.centerodds))
 	repeat
 		-- Add transforms to last shape
-		table.insert(shapes[#shapes]:getTransforms(),
-				randTransform(config.weights, config.continue,
-						config.translatemax, config.scalemax))
+		shapes[#shapes].transforms:extend(
+				randTransform(config.weights, config.moretransforms))
 		-- Pick a shape from the list to make the parent of the new shape
 		local parent = shapes[math.random(#shapes)]
-		table.insert(shapes, randShape(config.centerodds, parent))
+		shapes:append(randShape(config.centerodds, parent))
 		-- repeat
 	until math.random() < config.moreshapes
 	return shapes[1] -- returning root of scene, not the whole list
@@ -323,16 +346,14 @@ local function writeObjectSVG(shape)
 	-- Open a group
 	io.write('<g ')
 	-- Include all transforms
-	for k, v in ipairs(shape:getTransforms()) do
-		if v then
-			io.write(v:asSVGAttribute())
-		end
+	for k, v in ipairs(shape.transforms) do
+		io.write(v:asSVGAttribute())
 	end
 	io.write('>')
-	-- Include this specific object
+	-- Include specific given object...
 	shape:asSVGElement()
-	-- This object's children, recursively 
-	for k, v in ipairs(self:getChildren()) do
+	-- ...and given object's children, recursively 
+	for k, v in ipairs(shape.children) do
 		writeObjectSVG(v)
 	end
 	io.write('</g>')
@@ -367,29 +388,50 @@ end
 -- Seed RNG at start
 math.randomseed(os.time())
 
+
 -- define the main function
 local function main()
-	local args = parser:parse()
-	args.weights = {}
-	-- bundle it up more nicely for later passing
-	for k, v in pairs{"translate", "rotate", "scale", "skewx", "skewy"} do
-		args.weights[v] = args[v]
-		args[v] = nil
-	end
+	local valueFlags = {quantity = true,
+			moreshapes = true,
+			centerodds = true,
+			moretransforms = true,
+			translate = true,
+			scale = true,
+			rotate = true,
+			skewx = true,
+			skewy = true,
+			output = true,
+			help = true}
 
-	-- make directory if it doesn't exist
-	-- TODO add a tag to lock reading from the folder and prevent
-	-- overwriting old with new files by working from highest number
-	-- TODO base folder directly on args for organization
-	if not lfs.chdir(args.output) then
-		assert (lfs.mkdir(args.output), "Could not make or access target dir")
-		lfs.chdir(args.output)
-	end
-	
-	-- make all requested scenes
-	for i = 1, args.quantity do
-		local scene = makeScenegraph(args)
-		writeSceneSVG(scene, tostring(i) .. '.svg')
+	local flags, _ = pl.app.parse_args(nil, valueFlags)
+	if #_ > 0 then
+		help()
+	else
+		local weights = {}
+		-- bundle it up more nicely for later passing
+		for t in pl.List.iterate{"translate", "rotate", "scale", "skewx", "skewy"} do
+			weights[t] = tonumber(flags[t])
+		end
+
+		-- TODO add a tag to lock reading from the folder and prevent
+		-- overwriting old with new files by working from highest number
+		-- TODO base folder directly on args for organization
+		if not pl.path.exists(flags.output) then
+			assert (pl.dir.makepath(flags.output),
+				"Could not make or access target dir")
+		end
+		
+		-- make all requested scenes
+		for i = 1, flags.quantity do
+			local scene = makeScenegraph {
+				moreshapes = tonumber(flags.moreshapes),
+				centerodds = tonumber(flags.centerodds),
+				moretransforms = tonumber(flags.moretransforms),
+				weights = weights}
+			writeSceneSVG(scene,
+					pl.path.join(flags.output,
+							tostring(i) .. '.svg'))
+		end
 	end
 end
 
