6ffe9f5b4a8fdd62bc6a7f568b4b97917403273d
David Heyman <d.heyman256@gmail.com>
2016-12-16 23:38:15 -0500

    Added testrun.sh for example command

---
+++ testrun.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Example command, arbitrary-but-valid parameters
+lua logoglyph.lua --quantity 1000 --moreshapes 0.6 --centerodds 0.5 --moretransforms 0.6 --translate 1 --rotate 1 --scale 1 --skewx 1 --skewy 1 --output first
